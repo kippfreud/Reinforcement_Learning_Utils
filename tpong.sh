1e8689b9f7a2d68335ccb640d67389047af92d0c
tombewley <tom.bewley@bristol.ac.uk>
2021-03-05 16:15:00 +0000

    Update .sh file

--- tpong.sh
+++ tpong.sh
@@ -4,9 +4,9 @@
 #SBATCH --time=3-12:00:00
 #SBATCH --partition gpu
 #SBATCH --mem=16G
-#SBATCH --job-name=AE_0
+#SBATCH --job-name=train_pong
 module load CUDA
 module load apps/torch/28.01.2019
 module load languages/anaconda3/3.6.5
-echo "AE_0"
-python3 train_pong.py
+echo "Training on Pong..."
+python3 train_pong.py > train_pong.out

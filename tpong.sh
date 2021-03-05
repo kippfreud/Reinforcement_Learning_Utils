6e4de01a0bd4b1b5f37c5c1de88b2e0956262d5c
Tom Bewley <bewster1@gmail.com>
2021-03-05 17:09:44 +0000

    Stripping down .sh script

--- tpong.sh
+++ tpong.sh
@@ -5,8 +5,5 @@
 #SBATCH --partition gpu
 #SBATCH --mem=16G
 #SBATCH --job-name=train_pong
-module load CUDA
-module load apps/torch/28.01.2019
-module load languages/anaconda3/3.6.5
 echo "Training on Pong..."
 python3 train_pong.py > train_pong.out

589727c1793a5b0194221409c929bf7aa6d66606
tombewley <tom.bewley@bristol.ac.uk>
2021-03-10 16:41:36 +0000

    Import module behaviour

--- rlutils/__init__.py
+++ rlutils/__init__.py
@@ -1,5 +1,11 @@
+# Agent classes
 from .agents.dqn import DqnAgent
 from .agents.reinforce import ReinforceAgent
 from .agents.actor_critic import ActorCriticAgent
+from .agents.ddpg import DdpgAgent
+from .agents.stable_baselines import StableBaselinesAgent
+from .agents.random import RandomAgent
 
+# Common classes
 from .common.observer import Observer
+from .common.deployment import train, deploy
